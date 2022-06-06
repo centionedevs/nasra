a81d37daac4ca4958a58ef5850d173610f067f0b
Abo El Fadl <fodaseken1997@gmail.com>
2022-06-06 10:17:24 +0200

    updated

--- nasra_hr_employee_customizations/models/hr_employee.py
+++ nasra_hr_employee_customizations/models/hr_employee.py
@@ -15,6 +15,7 @@ class HrEmployee(models.Model):
     embassy_name = fields.Char('Embassy Name')
     social_insure_no = fields.Char('Social Insurance No')
     social_insure_office = fields.Char('Social Insurance Office')
+    
     social_insure_start = fields.Date('Insurance Start Date')
     social_insure_end = fields.Date('Insurance End Date')
     social_insure_vac_date = fields.Date('Social Insurance Date For Vacation Balance')
