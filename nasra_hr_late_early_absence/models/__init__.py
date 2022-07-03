56778ba537951d058e7bd4bd27e27f67c96a32c9
Abo El Fadl <fodaseken1997@gmail.com>
2022-07-03 15:23:24 +0200

    updated modules

--- nasra_hr_late_early_absence/models/__init__.py
+++ nasra_hr_late_early_absence/models/__init__.py
@@ -8,3 +8,4 @@ from . import absence_penalty_line
 from . import late_early_penalty_line
 from . import late_early_time_interval
 from . import resource_calendar_leaves
+from . import hr_employee
---
+++ nasra_hr_organization_chart/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    'name': 'Park Organization Chart',
+    'version': '1.0',
+    'category': 'Human Resource',
+    'summary': 'Nasra Organization Chart',
+    'description': """
+Nasra Organization Chart:
+=======================================================
+
+* To be listed
+    """,
+    'author': 'Centione',
+    'website': 'https://www.centione.com',
+    'depends': [ 'hr'],
+    'data': [
+
+          'views/template.xml',
+          'views/views.xml',
+    ],
+    'qweb': [
+        'static/src/xml/capital_templates.xml',
+
+    ],
+    'installable': True,
+    'auto_install': False,
+}
