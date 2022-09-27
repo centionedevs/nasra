37ba26af0ebb363a83489f54641e6e45262e55dc
karem-mousa <karem1aref@gmail.com>
2022-09-27 11:15:53 +0200

    [ADD] Employee File Code

--- nasra_hr_late_early_absence/models/hr_employee.py
+++ nasra_hr_late_early_absence/models/hr_employee.py
@@ -6,3 +6,4 @@ class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
     late_early_absence_rule = fields.Boolean('Late Early Absence Apply Rule')
+    employee_file_code = fields.Char(string="Employee File Code", required=False, )
